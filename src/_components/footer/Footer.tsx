90c73a3f4777912b42f2a5322bd5a9f8a5001a1e
BASIM THANI <mjnoonha90@gmail.com>
2025-04-29 02:27:53 +0300

    background img1

--- src/_components/footer/Footer.tsx
+++ src/_components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="w-full text-center py-4"> 
       <p>
-        <a className="text-[#dddddd] font-bold" href="mailto:recipient@example.com">hello@mohat.ai</a>
+        <a className="text-[#dddddd] font-bold text-lg" href="mailto:recipient@example.com">hello@mohat.ai</a>
       </p>
     </footer>
   )
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
+import Footer from '@/_components/footer/footer';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from "@/_components/footer/footer";
 
 const MainLayout: React.FC = () => {
   return (
