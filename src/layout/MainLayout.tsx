740492db34e5e272a95c1a9a9e910b90825ec158
BASIM THANI <mjnoonha90@gmail.com>
2025-04-29 02:34:53 +0300

    background img2

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout: React.FC = () => {
   return (
-    <main className=' bg-img w-full bg-[#ffffff]'>
+    <main className='bg-img w-full bg-[#ffffff]'>
       <div>
         <Outlet />
       </div>
