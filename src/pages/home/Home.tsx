655d26e950faeeb5d47eb6388448c99d1827d9f6
BASIM THANI <mjnoonha90@gmail.com>
2025-04-29 02:42:06 +0300

    upgrade

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,9 +2,9 @@
 
 const Home: React.FC = () => {
   return (
-    <div className="w-screen my-[-28px] min-h-screen flex items-center justify-center flex-col">
-      <h1 className="w-full text-3xl md:text-5xl font-bold mb-8 text-center text-[#dddddd]">
-        Coming Soon!
+    <div className="w-screen my-[-28px] min-h-screen pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)] flex items-center justify-center flex-col">
+      <h1 className="w-full text-3xl md:text-4xl font-bold mb-8 text-center text-[#dddddd]">
+        Coming Soon
       </h1>
     </div>
   );
